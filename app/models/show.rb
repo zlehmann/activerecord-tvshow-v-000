e62549ee8d4e3916b5993c83952b8611c3660c24
Zachary Lehmann <zlehmann@gmail.com>
2018-11-15 23:38:21 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,13 @@
 require 'pry'
 class Show < ActiveRecord::Base
   def self.highest_rating
-    result = Show.maximum(:rating)
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.maximum(:rating).name
+
+  def self.lowest_rating
+    Show.minimum(:rating)
   end
 end
