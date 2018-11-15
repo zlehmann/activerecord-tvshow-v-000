da4578a1462cbe36482a20ffa7c81256377154a7
Zachary Lehmann <zlehmann@gmail.com>
2018-11-15 23:44:57 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -11,4 +11,20 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     Show.minimum(:rating)
   end
+
+  def self.least_popular_show
+    Show.where("rating = ?", Show.minmum(:rating)).first
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
+  def popular_shows
+    Show.where("rating > 5")
+  end
+
+  def shows_by_alphabetical_order
+    Show.order(:name)
+  end
 end
